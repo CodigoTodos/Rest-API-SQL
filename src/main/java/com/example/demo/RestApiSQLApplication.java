7bcf86bc5224dc526dccdd850fe2e7ab8ae0036c
Bruno Alves <bmalves@criticalsoftware.com>
2022-04-05 17:39:52 +0100

    REST API SQL

--- src/main/java/com/example/demo/RestApiSQLApplication.java
+++ src/main/java/com/example/demo/RestApiSQLApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestApiH2Application {
+public class RestApiSQLApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestApiH2Application.class, args);
+		SpringApplication.run(RestApiSQLApplication.class, args);
 	}
 
 }
